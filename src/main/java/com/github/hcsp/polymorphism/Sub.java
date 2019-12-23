9e2d9c53f75e0924b159552febd542b41d7d7ab9
Xi Pu <xp19@duke.edu>
2019-12-23 15:36:24 -0800

    change sub

--- src/main/java/com/github/hcsp/polymorphism/Sub.java
+++ src/main/java/com/github/hcsp/polymorphism/Sub.java
@@ -1,10 +1,10 @@
 package com.github.hcsp.polymorphism;
 
 public class Sub extends Base {
-    @Override
-    public void print(ParamBase param) {
-        System.out.println("I am Sub, the param is ParamBase");
-    }
+//    @Override
+//    public void print(ParamBase param) {
+//        System.out.println("I am Sub, the param is ParamBase");
+//    }
 
     @Override
     public void print(ParamSub param) {
