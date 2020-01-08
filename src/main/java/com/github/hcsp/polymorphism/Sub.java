38e010209ff25a12406b22836f6ea79247a459ab
spclock <1124516293@qq.com>
2020-01-08 16:19:53 +0800

    选择正确的重载方法

--- src/main/java/com/github/hcsp/polymorphism/Sub.java
+++ src/main/java/com/github/hcsp/polymorphism/Sub.java
@@ -2,9 +2,7 @@ package com.github.hcsp.polymorphism;
 
 public class Sub extends Base {
     @Override
-    public void print(ParamBase param) {
-        System.out.println("I am Sub, the param is ParamBase");
-    }
+    public void print(ParamBase param) { System.out.println("I am Sub, the param is ParamBase"); }
 
     @Override
     public void print(ParamSub param) {
