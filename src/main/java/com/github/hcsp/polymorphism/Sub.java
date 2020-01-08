f4d22acb990524e2b6b8343f8c3f674326318dc8
spclock <1124516293@qq.com>
2020-01-08 16:26:13 +0800

    我就点了一下Sub类就给格式化了

--- src/main/java/com/github/hcsp/polymorphism/Sub.java
+++ src/main/java/com/github/hcsp/polymorphism/Sub.java
@@ -2,7 +2,9 @@ package com.github.hcsp.polymorphism;
 
 public class Sub extends Base {
     @Override
-    public void print(ParamBase param) { System.out.println("I am Sub, the param is ParamBase"); }
+    public void print(ParamBase param) {
+        System.out.println("I am Sub, the param is ParamBase");
+    }
 
     @Override
     public void print(ParamSub param) {
