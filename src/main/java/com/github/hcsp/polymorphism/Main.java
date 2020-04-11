a80aac90a525e82227f2ae74ad778aae6c21a234
Ma Haoxiang <61976611+strivejojo@users.noreply.github.com>
2020-04-11 22:32:27 +0800

    Java面向对象初步练习：选择正确的重载方法 (#152)
    Java面向对象初步练习：选择正确的重载方法 (#152)

--- src/main/java/com/github/hcsp/polymorphism/Main.java
+++ src/main/java/com/github/hcsp/polymorphism/Main.java
@@ -2,11 +2,12 @@ package com.github.hcsp.polymorphism;
 
 public class Main {
     // 猜一下输出结果是什么，然后运行一下看看是不是和你预期相符
+    //  I am Sub, the param is ParamBase
     // 请修改下面的代码，使之输出
     // "I am Sub, the param is ParamSub"
     public static void main(String[] args) {
         Base object = new Sub();
-        ParamBase param = new ParamSub();
+        ParamSub param = new ParamSub();
         object.print(param);
     }
 }
